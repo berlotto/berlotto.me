4e9c8afeb5b2bb3d8795929ae6db2ae814fb4875
Sérgio Berlotto <sergio@jetbov.com>
2024-07-04 23:13:50 -0300

    Novo post e icons plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,9 @@ import sitemap from "@astrojs/sitemap";
 import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel/serverless';
 import { remarkReadingTime } from './plugins/remark-reading-time.mjs';
+import astroD2 from 'astro-d2';
 
-import astroD2 from 'astro-d2'
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,7 +20,7 @@ export default defineConfig({
   // replace this with your deployed domain
   integrations: [tailwind({
     applyBaseStyles: false
-  }), react(), sitemap(), mdx(), astroD2()],
+  }), react(), sitemap(), mdx(), astroD2(), icon()],
   markdown: {
     remarkPlugins: [remarkReadingTime, remarkToc, [remarkCollapse, {
       test: "Índice"
